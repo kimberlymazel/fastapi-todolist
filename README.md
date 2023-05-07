ed0d8f5fc82055b618f366f285572bb09014f048
kim <91533432+kimberlymazel@users.noreply.github.com>
2023-05-07 17:15:28 +0700

    added user methods and updated docs

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# fastapi-todolist
-This is a REST API created using Fast API for a To-Do List.
+<h1 align="center">RESTful To-Do List using Fast API</h1>
+This is a REST API created using Fast API for a To-Do List with multi-user support.
+
 The design of the API is seen as followed.
 
-![design](docs/design.png)
+## Task Endpoint Design
+![task_designs](docs/task_design.png)
 
+## User Endpoint Design
+![user_designs](docs/user_design.png)
+
+## File Directory
 The .py file containing the codes can be found in the [virtual environment](venv) folder named as [main.py](venv/main.py).
 
 The requirements needed to run the program can be found in the [requirements](requirements.txt) file.
 
+## SwaggerUI Documentation
+
 The API endpoints provided by the code is summarized through the Swagger UI documentation below.
 
-![swagger docs](docs/swaggerui.png)
+![swaggerui](docs/swagger_docs.png)
--- venv/main.py
+++ venv/main.py
@@ -4,62 +4,196 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+## ----------------------------------------------------------- ##
+## ----------------------------------------------------------- ##
+## ----------------------- BASE MODELS ----------------------- ##
+## ----------------------------------------------------------- ##
+## ----------------------------------------------------------- ##
+
+class User(BaseModel):
+    username: str
+    email: str
+    auth: str
+
+class UpdateUser(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    auth: Optional[str] = None
+
 class Task(BaseModel):
     name: str
     description: str
     status: str
+    username: str
 
 class UpdateTask(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
     status: Optional[str] = None
+    username: Optional[str] = None
 
-# Filler Tasks
+## ---------------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+## ---------------------- DICTIONARIES ---------------------- ##
+## ---------------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+
 # Main list of todo, all tasks are put here
 tasklist = {
     1: Task(
         name = "Walk the dog",
         description = "Don't forget to walk the dog at 10 A.M.",
-        status = "Incompleted"
+        status = "Incompleted",
+        username = "kimberlymazel"
     ),
 
     2: Task(
         name = "Laundry",
         description = "Wash uniforms before school on Monday",
-        status = "Incompleted"
+        status = "Incompleted",
+        username = "kimberlymazel"
     ),
 
     3: Task(
         name = "Dishes",
         description = "Don't forget to put them in the dryer",
-        status = "Completed"
+        status = "Completed",
+        username = "bagzmate"
+    )
+}
+
+# Main list of users, all users are put here
+userlist = {
+    1: User(
+        username = "kimberlymazel",
+        email = "km@gmail.com",
+        auth = "google"
+    ),
+
+    2: User(
+        username = "bagzmate",
+        email = "bagzmate@binus.ac.id",
+        auth = "github"
     )
 }
 
 # List for filtered tasks
-# Filtered based on status
-# Used for FILTER BY STATUS method
 filteredlist = {
     1: Task(
         name = "Filtered",
         description = "Filtered",
-        status = "Filtered"
+        status = "Filtered",
+        username = "Filtered"
     ),
 }
 
-# GET METHOD
+# List for filtered users
+filteredusers = {
+    1: User(
+        username = "filtered",
+        email = "filtered",
+        auth = "filtered"
+    )
+}
+
+# INDEX
 @app.get("/")
 def index():
     return {"Hello": "This is a To-Do List made by FastAPI"}
 
+## ---------------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+## ---------------------- USER METHODS ---------------------- ##
+## ---------------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+
+# GET ALL USERS
+@app.get("/get-userlist/")
+def get_all_users():
+    return userlist
+
+# GET USER BY ID
+@app.get("/get-user/{user_id}")
+def get_user(user_id: int = Path(description="ID of User")):
+    return userlist[user_id]
+
+# GET USER BY USERNAME
+@app.get("/get-user-by-username/{username}")
+def get_user(username: str):
+    for user_id in userlist:
+        if userlist[user_id].username == username:
+            return userlist[user_id]
+    return {"Error":"User does not exist"}
+
+# FILTER USER BY AUTHENTICATION
+# Example: List all users from google
+@app.get("/filter-by-auth/{auth}")
+def get_user(auth: str):
+    global filteredusers # Global to avoid UnboundLocalError 
+    filteredusers.clear() # Clear list (clear filter)
+
+    # Traverses through all users
+    for user_id in userlist:
+
+        # If current user's auth is the auth that is requested
+        # Current user is appended to filteredusers
+        if userlist[user_id].auth == auth:
+            filteredusers[user_id] = userlist[user_id]
+
+    # Return filteredusers after all tasks are checked
+    return filteredusers
+
+# ADD NEW USER
+@app.post("/add-user/")
+def add_user(user_id: int, user : User):
+    if user_id in userlist:
+        return {"Error" : "User already exists"}
+    userlist[user_id] = user
+    return userlist[user_id]
+
+# UPDATE USER (USERNAME, EMAIL, AUTH)
+@app.put("/update-user/{user_id}")
+def update_user(user_id: int, user: UpdateUser):
+    if user_id not in userlist:
+        return {"Error":"User does not exist"}
+
+    if user.username != None:
+        userlist[user_id].username = user.username
+
+    if user.email != None:
+        userlist[user_id].email = user.email
+
+    if user.auth != None:
+        userlist[user_id].auth = user.auth
+    
+    return userlist[user_id]
+
+# DELETE USER
+@app.delete("/delete-user/{user_id}")
+def delete_user(user_id: int):
+    if user_id not in userlist:
+        return {"Error":"The user does not exist."}
+
+    del userlist[user_id]
+    return{"User":"Deleted successfully."}
+
+## ---------------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+## ---------------------- TASK METHODS ---------------------- ##
+## ---------------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+
+# GET ALL TASKS
+@app.get("/get-tasklist/")
+def get_all_task():
+    return tasklist
+
 # GET TASK BY ID
-# GET METHOD
 @app.get("/get-task/{task_id}")
 def get_task(task_id: int = Path(description="ID of Task")):
     return tasklist[task_id]
 
 # GET TASK BY NAME
-# GET METHOD
 @app.get("/get-task-by-name/{name}")
 def get_task(name: str):
     for task_id in tasklist:
@@ -68,7 +202,6 @@ def get_task(name: str):
     return {"Error":"Task does not exist"}
 
 # FILTER BY STATUS
-# GET METHOD
 # Example: List all tasks that are completed.
 @app.get("/filter-by-status/{status}")
 def get_task(status: str):
@@ -86,8 +219,25 @@ def get_task(status: str):
     # Return filteredlist after all tasks are checked
     return filteredlist
 
+# FILTER BY USERNAME
+# Example: List all tasks that are by user1.
+@app.get("/filter-by-user/{username}")
+def get_task(username: str):
+    global filteredlist # Global to avoid UnboundLocalError 
+    filteredlist.clear() # Clear list (clear filter)
+
+    # Traverses through all tasks
+    for task_id in tasklist:
+
+        # If current task's username is the username that is requested
+        # Current task is appended to the filteredlist
+        if tasklist[task_id].username == username:
+            filteredlist[task_id] = tasklist[task_id]
+
+    # Return filteredlist after all tasks are checked
+    return filteredlist
+
 # ADD NEW TASK
-# POST METHOD
 @app.post("/add-task/")
 def add_task(task_id: int, task : Task):
     if task_id in tasklist:
@@ -95,8 +245,7 @@ def add_task(task_id: int, task : Task):
     tasklist[task_id] = task
     return tasklist[task_id]
 
-# UPDATE TASK (NAME, DESC, STATUS)
-# PUT METHOD
+# UPDATE TASK (NAME, DESC, STATUS, USERNAME)
 @app.put("/update-task/{task_id}")
 def update_task(task_id: int, task: UpdateTask):
     if task_id not in tasklist:
@@ -110,10 +259,13 @@ def update_task(task_id: int, task: UpdateTask):
 
     if task.status != None:
         tasklist[task_id].status = task.status
+
+    if task.username != None:
+        tasklist[task_id].username =  task.username
     
     return tasklist[task_id]
 
-# DELETE METHOD
+# DELETE TASK
 @app.delete("/delete-task/{task_id}")
 def delete_task(task_id: int):
     if task_id not in tasklist:
